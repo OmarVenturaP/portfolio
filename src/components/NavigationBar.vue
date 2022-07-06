230a842177697162d13fd7ce3a57f53d2dba45cc
Omar Ventura <99059639+OmarVenturaP@users.noreply.github.com>
2022-07-06 17:01:01 -0500

    Creando barra de navegación

---
+++ src/components/NavigationBar.vue
@@ -0,0 +1,31 @@
+<template>
+    <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
+        <div class="container">
+            <a class="navbar-brand" href="#page-top"><img class="logo" src="../assets/img/navbar-logo2.svg" alt="..." /></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                Menu
+                <i class="fas fa-bars ms-1"></i>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav text-uppercase ms-auto py-4 py-lg-0">
+                    <li class="nav-item"><a class="nav-link" href="#services">Tecnologías</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#portfolio">Portfolio</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#about">Formacion</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#team">sobre mi</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#certified">Certificaciones</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#contact">Contacto</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<script>
+export default {
+    name: 'NavBar',
+}
+</script>
+
+<style>
+
+</style>
